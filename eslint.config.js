862ab269643f0c7b15a76988ac3f1af978f6160e
markcodecode825-coder <markcodecode825@gmail.com>
2026-09-21 21:11:29 -0300

    fix: exclude CommonJS tool configs from lint

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ const js = require('@eslint/js');
 const tseslint = require('typescript-eslint');
 
 module.exports = tseslint.config(
-  { ignores: ['dist/**', 'node_modules/**', 'coverage/**'] },
+  { ignores: ['dist/**', 'node_modules/**', 'coverage/**', 'eslint.config.js', 'prettier.config.js'] },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
