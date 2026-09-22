b242b55a1e09ab9c753280da8cccb5c2e94a20b3
markcodecode825-coder <markcodecode825@gmail.com>
2026-09-21 21:08:00 -0300

    fix: align ESLint rules with Fastify TypeScript project

--- eslint.config.js
+++ eslint.config.js
@@ -4,18 +4,12 @@ const tseslint = require('typescript-eslint');
 module.exports = tseslint.config(
   { ignores: ['dist/**', 'node_modules/**', 'coverage/**'] },
   js.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.recommended,
   {
-    languageOptions: {
-      parserOptions: {
-        project: './tsconfig.json',
-        tsconfigRootDir: __dirname
-      }
-    },
     rules: {
-      '@typescript-eslint/no-misused-promises': ['error', { checksVoidReturn: false }],
       '@typescript-eslint/consistent-type-imports': 'error',
-      '@typescript-eslint/no-explicit-any': 'error'
+      '@typescript-eslint/no-explicit-any': 'error',
+      'no-control-regex': 'off'
     }
   }
 );
