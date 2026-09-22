48ff67ec3a8c999b68849f9ba752ac809abd7dbb
markcodecode825-coder <markcodecode825@gmail.com>
2026-09-22 09:34:52 -0300

    chore: bump API version to 1.3.0

--- src/plugins/swagger.ts
+++ src/plugins/swagger.ts
@@ -10,7 +10,7 @@ const swaggerPlugin: FastifyPluginAsync = async (app) => {
       info: {
         title: 'Top Lanches API',
         description: 'API REST oficial e open source da Top Lanches.',
-        version: '1.2.0',
+        version: '1.3.0',
         license: { name: 'MIT' }
       },
       tags: [
